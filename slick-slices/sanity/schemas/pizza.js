ec58ae9dd37a63eebdf3806a96c6c97f18fd7299
Troy Porter <porter@fb.com>
2020-10-04 12:36:37 +0100

    4.13 Creating Data Relationships

--- slick-slices/sanity/schemas/pizza.js
+++ slick-slices/sanity/schemas/pizza.js
@@ -39,5 +39,40 @@ export default {
       validation: (Rule) => Rule.min(1000),
       // TODO: Add custom input component
     },
+    {
+      name: 'toppings',
+      title: 'Toppings',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {
+              type: 'topping',
+            },
+          ],
+        },
+      ],
+    },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping_0: 'toppings.0.name',
+      topping_1: 'toppings.1.name',
+      topping_2: 'toppings.2.name',
+      topping_3: 'toppings.3.name',
+    },
+    prepare: ({ title, media, ...toppings }) => {
+      // 1. Filter out undefined toppings
+      const tops = Object.values(toppings).filter(Boolean);
+      // 2. Return the preview object for the pizza
+      return {
+        title,
+        media,
+        subtitle: Object.values(tops).join(', '),
+      };
+    },
+  },
 };
