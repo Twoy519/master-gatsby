50a375739c5f20669f302fed31a82f327190440b
Troy Porter <porter@fb.com>
2020-10-17 22:10:16 +0100

    5.18 Learning Gatsby Queries and making Pizza Components

--- slick-slices/gatsby/src/pages/pizzas.js
+++ slick-slices/gatsby/src/pages/pizzas.js
@@ -1,9 +1,42 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
+export default function PizzasPage({ data }) {
+  // no loading state in Gatsby. SSR won't render until its there.
+  const pizzas = data.pizzas.nodes;
   return (
     <>
-      <p>Hey! I'm the Pizzas page</p>
+      <PizzaList pizzas={pizzas} />
     </>
   );
 }
+
+// page-level query
+export const pageQuery = graphql`
+  query PizzaQuery {
+    # rename to just pizzas
+    pizzas: allSanityPizza {
+      nodes {
+        id
+        name
+        slug {
+          _key
+          _type
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
